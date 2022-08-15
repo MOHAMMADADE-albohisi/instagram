1c1ba331e493d15db973ab848c35d1edfc224944
MOHAMMADADE-albohisi <mohammadalbohisi2@gmail.com>
2022-08-15 16:56:51 +0300

    commit

--- lib/screen/home_scrren.dart
+++ lib/screen/home_scrren.dart
@@ -1,10 +1,5 @@
-import 'dart:ui' show FontWeight, TextAlign, TextDecoration;
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:instagram/widgets/post_widget.dart';
-import 'package:instagram/widgets/story_widget.dart';
 
 class homescrren extends StatefulWidget {
   const homescrren({Key? key}) : super(key: key);
@@ -65,15 +60,88 @@ class _homescrrenState extends State<homescrren> {
                   scrollDirection: Axis.horizontal,
                   child: Row(
                     children: const [
-                      story_widget(
-                          image:
-                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ89EQRdWUQNNmQnn5aF1CpoiwTw5266k0lMw&usqp=CAU'),
-                      story_widget(
-                          image:
-                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrADcrPCjamY_cvW_JicPN15A25vNw_JCQNw&usqp=CAU'),
-                      story_widget(
-                          image:
-                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVqsPRSpyJwiREta_wW7qyzdPU4AgLSaALPP80-qEqg4EXFymFQSadafQeht-9SOIdHjU&usqp=CAU')
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                            'https://media.istockphoto.com/photos/portrait-of-creative-trendy-black-african-male-designer-laughing-picture-id1296158947?k=20&m=1296158947&s=612x612&w=0&h=qh89sBTcLTUuuOZMMUwMrbfnWqERRyZch9R6Z74S9Lc='),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://ichef.bbci.co.uk/news/976/cpsprodpb/1532F/production/_122913868_dulcie.jpg',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1zmQAGJV2ppXb3KuvxsdEiXkuEoVH6eY7f15o1orBjvmzKQCq2yKzKNgkECLfJYWZaqc&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTalnndvRgqpuYMS0KAgmJ0YlPiqtAf0zB1yw&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfr0Bn77-ma4Ag94Ya_8pvcaZbg5bbZSnDhQ&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://media.istockphoto.com/photos/young-beautiful-couple-sitting-using-laptop-around-christmas-at-home-picture-id1185931417?k=20&m=1185931417&s=612x612&w=0&h=PtW3n_1seqclhQXZiaBHv-pSAWprkx0-Px0Q_XFA1u8=',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbVGKxAdKOko2ETHFpe8MKMP43g4ciiZNxPg&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVvcGxlJTIwZnVsbCUyMGJvZHl8ZW58MHx8MHx8&w=1000&q=80',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe7jFCjNyxCt4-33FX1Y_wwBBUf117tv-3LA&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ89EQRdWUQNNmQnn5aF1CpoiwTw5266k0lMw&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrADcrPCjamY_cvW_JicPN15A25vNw_JCQNw&usqp=CAU',
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVqsPRSpyJwiREta_wW7qyzdPU4AgLSaALPP80-qEqg4EXFymFQSadafQeht-9SOIdHjU&usqp=CAU',
+                        ),
+                      ),
                     ],
                   ),
                 ),
